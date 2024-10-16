70fc65dc68de1a362484d041fd52ef33a7595770
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-10-16 16:42:29 +0200

    query  6

--- university_query.sql
+++ university_query.sql
@@ -33,6 +33,11 @@ WHERE `date` = '20-06-2020'
 AND `hour` >= '14.00.00';
 
 /*6. Selezionare tutti i corsi di laurea magistrale (38)  */
+
+SELECT * FROM `degrees` 
+WHERE `level` = "magistrale";
+
+
 /*7. Da quanti dipartimenti è composta l'università? (12) */
 
 /*8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50) */
