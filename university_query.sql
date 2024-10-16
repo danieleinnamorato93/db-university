90fe7ffdcf7e482245e69d02a196d7fa756e3631
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-10-16 16:38:13 +0200

    query 5

--- university_query.sql
+++ university_query.sql
@@ -23,8 +23,15 @@ SELECT*
 FROM `courses`
 WHERE `period` = "I semestre"
 AND `year` = 1;
+
 /*5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del
 20/06/2020 (21)*/
+
+SELECT*
+FROM  `exams`
+WHERE `date` = '20-06-2020'
+AND `hour` >= '14.00.00';
+
 /*6. Selezionare tutti i corsi di laurea magistrale (38)  */
 /*7. Da quanti dipartimenti è composta l'università? (12) */
 
