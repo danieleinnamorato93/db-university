eec991c9937550a8d8eaacb109eb60b8afde1228
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-10-17 17:51:01 +0200

    query_4

--- query_join.sql
+++ query_join.sql
@@ -23,6 +23,11 @@ WHERE `teachers`.`id` = 44;
 
 
 --4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome nome
+SELECT*
+FROM `students`
+JOIN `degrees` ON `students`.`degree_id` = `degrees`.`id`
+JOIN `departments` ON `degrees`.`department_id` = `departments`.`id`
+ORDER BY `students`.`surname`,`students`.`name`;
 
 
 --5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
